bc929003e34007737a6cf1cf8a953609e7c98c87
pipacsba <44190358+pipacsba@users.noreply.github.com>
2026-01-15 09:14:09 +0100

    Remove base HTML tag from main_page function
    Removed unused base HTML tag from main page.

--- main.py
+++ main.py
@@ -32,11 +32,6 @@ state = load_data()
 
 @ui.page('/')
 def main_page():
-    ui.add_head_html('''
-         <base href="{{ request.script_root }}/">
-         ''')
-    
-    
     ui.dark_mode().enable()
     
     with ui.card().classes('w-full max-w-lg mx-auto p-6'):
