a44e5770fd102b04f865823fe0d8f234a037069e
pipacsba <44190358+pipacsba@users.noreply.github.com>
2026-01-22 11:15:28 +0100

    Update navigation.py

--- ui/navigation.py
+++ ui/navigation.py
@@ -36,7 +36,7 @@ def build_header():
         with ui.icon('circle', color=f'{a_color}').classes('text-5xl'):
             if a_color == 'green':
                 ui.tooltip('No upload is pending').classes('bg-green')
-            else
+            else:
                 ui.tooltip('Upload is pending').classes('bg-red')
 
 # -------------------
