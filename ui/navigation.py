4b6713537dce4a7fb78b51c60b62d83717ed17f7
pipacsba <44190358+pipacsba@users.noreply.github.com>
2026-01-25 17:56:05 +0100

    Update navigation.py

--- ui/navigation.py
+++ ui/navigation.py
@@ -38,6 +38,8 @@ def pending_ui_refresh():
   pending_ui.refresh()
         
 change_upload_is_pending.add_observer(pending_ui_refresh)
+client = ui.context.client
+client.on_disconnect(lambda: servers_online.remove_observer(on_servers_changed))
 
 def refresh_ssh_sync():
     ui.notify("SSH syncronziation is triggered")
