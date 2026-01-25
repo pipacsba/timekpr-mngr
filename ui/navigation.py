59768c05dd013c0bff92f4332b293000b3917aeb
pipacsba <44190358+pipacsba@users.noreply.github.com>
2026-01-25 10:10:04 +0100

    Update navigation.py

--- ui/navigation.py
+++ ui/navigation.py
@@ -52,9 +52,8 @@ def build_header():
         ui.link('pty', '/pty').classes('font-bold text-brand')
         ui.link('browse_folders', '/browse_folders').classes('font-bold text-brand')
         ui.space()
-        with ui.link(target=trigger_ssh_sync):
-                with ui.icon('refresh', color=f'green').classes('text-5xl'):
-                        ui.tooltip(f'Reload server info').classes(f'green')
+        with ui.icon('refresh', color=f'green').on('click', my_function).classes('text-5xl cursor-pointer'):
+             ui.tooltip(f'Reload server info').classes(f'green')
         pending_ui()
 
 # -------------------
