0f1fcfea9d56187c418fbd6fe52106640dddfef6
pipacsba <44190358+pipacsba@users.noreply.github.com>
2026-01-25 15:08:20 +0100

    Update navigation.py

--- ui/navigation.py
+++ ui/navigation.py
@@ -1,7 +1,7 @@
 # ui/navigation.py
 from nicegui import ui
 from servers import load_servers, list_users
-from ssh_sync import change_upload_is_pending, trigger_ssh_sync, servers_online
+from ssh_sync import change_upload_is_pending, trigger_ssh_sync
 from ui.servers_page import servers_page
 from ui.config_editor import render_config_editor
 from ui.stats_dashboard import render_stats_dashboard
@@ -38,7 +38,6 @@ def pending_ui_refresh():
   pending_ui.refresh()
         
 change_upload_is_pending.add_observer(pending_ui_refresh)
-servers_online.add_observer(
 
 def refresh_ssh_sync():
     ui.notify("SSH syncronziation is triggered")
