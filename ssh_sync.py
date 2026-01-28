9fbf1f037b805d21295d49937bcd61d67c5c2c68
pipacsba <44190358+pipacsba@users.noreply.github.com>
2026-01-28 13:14:49 +0100

    Update ssh_sync.py

--- ssh_sync.py
+++ ssh_sync.py
@@ -238,7 +238,7 @@ def register_server_sensors(server: str):
             "platform": "binary_sensor",
             "qos": 1,
         },
-        "platform"= "binary_sensor",
+        platform = "binary_sensor",
     )
 
 def register_user_sensors(server: str, user: str):
@@ -252,7 +252,7 @@ def register_user_sensors(server: str, user: str):
             "device_class": "duration",
             "unique_id": f"timekpr_{server}_{user}_time",
         },
-        "platform"= "sensor",
+        platform = "sensor",
     )
 
     publish_ha_sensor(
@@ -265,7 +265,7 @@ def register_user_sensors(server: str, user: str):
             "device_class": "duration",
             "unique_id": f"timekpr_{server}_{user}_playtime",
         },
-        "platform": "sensor",
+        platform = "sensor",
     )
 
 def _update_user_history(server: str, user: str, stats_file: Path, updated: bool) -> None:
